4c33fa15309ced4c04c1ccc0ac8a56d6b0f52b0d
K02D <kirondeb02@gmail.com>
2022-12-12 18:59:03 -0500

    Follow lectures on routing

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(), // Make sure browser back button works
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+    {
+      path: '/teams/:teamId',
+      component: TeamMembers,
+    },
+  ],
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#app');
